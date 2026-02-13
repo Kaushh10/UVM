dedb95c7cd1d7a810409ea022e8b4c6074724797
Kaushal Tapray <144718681+Kaushh10@users.noreply.github.com>
2026-02-13 22:13:09 +0530

    Create package.sv

---
+++ ALU/alu_tb/package.sv
@@ -0,0 +1,16 @@
+package alu_stuff;
+
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+`include "seq_item.sv"
+`include "sequence.sv"
+`include "sequencer.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+`include "agent.sv"
+`include "environment.sv"
+`include "test.sv"
+
+endpackage : alu_stuff
